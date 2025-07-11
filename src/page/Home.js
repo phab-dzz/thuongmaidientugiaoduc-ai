b922d57a9a46fa0631a788ef5fc0b35136bc8032
PhanDe <duongvanphanls@gmail.com>
2025-07-11 20:55:39 +0700

    add page Home

---
+++ src/page/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div  className='flex  items-center h-full w-full gap-4 flex-col'>
+        <h1 >Welcome to the Home Page</h1>
+        <p>This is the starting point of our application.</p>
+        </div>
+    );
+    }
+export default Home;
